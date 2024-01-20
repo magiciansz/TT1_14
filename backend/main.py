2f0a29d3b64e6f163304d7a1cc8e8e1a8576f215
joel <joeltan999@gmail.com>
2024-01-20 10:41:30 +0800

    add itinerary router

--- backend/main.py
+++ backend/main.py
@@ -19,6 +19,7 @@ app.add_middleware(
 app.include_router(user_router, prefix='/user')
 app.include_router(user_router, prefix='/destination')
 app.include_router(auth_router, prefix='/auth')
+app.include_router(auth_router, prefix='/itinerary')
 
 @app.get("/")
 def root():
