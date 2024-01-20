7fc67eb83d6699c2fa5e35b84d4f032bf16e58bd
magiciansz <80561550+magiciansz@users.noreply.github.com>
2024-01-20 14:07:20 +0800

    add user_id and finish auth api

--- backend/app/dependencies/auth_handler.py
+++ backend/app/dependencies/auth_handler.py
@@ -10,15 +10,15 @@ JWT_SECRET = os.getenv("AUTH_SECRET_KEY")
 JWT_ALGORITHM = os.getenv("AUTH_ALGORITHM")
 
 
-def token_response(token: str):
-    return {"access_token": token}
+def token_response(token: str, user_id: int):
+    return {"access_token": token, "user_id": user_id}
 
 
 def signJWT(user_id: str) -> Dict[str, str]:
     payload = {"user_id": user_id, "expires": time.time() + 600}
     token = jwt.encode(payload, JWT_SECRET, algorithm=JWT_ALGORITHM)
 
-    return token_response(token)
+    return token_response(token, user_id)
 
 
 def decodeJWT(token: str) -> dict:
--- backend/app/routers/users.py
+++ backend/app/routers/users.py
@@ -15,9 +15,3 @@ JWT_ALGORITHM = os.getenv("AUTH_ALGORITHM")
 async def read_users(client: Client = Depends(get_supa_client)):
     res = client.table("users").select("*").execute()
     return res
-
-@router.get("/test")
-async def test_token(auth_token):
-    decoded_token = jwt.decode(auth_token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
-        # return decoded_token if decoded_token["expires"] >= time.time() else None
-    return decoded_token 
