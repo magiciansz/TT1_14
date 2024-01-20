71c806350d1d334cfff8debe7df481ff898d484f
magiciansz <80561550+magiciansz@users.noreply.github.com>
2024-01-20 13:09:32 +0800

    added auth code

--- backend/app/dependencies/auth_handler.py
+++ backend/app/dependencies/auth_handler.py
@@ -1,9 +1,58 @@
 
+# import time
+# from typing import Dict
+# from fastapi import Depends
+
+# import jwt
+# import os
+# from supabase import Client
+# from app.dependencies import get_supa_client
+# from fastapi.security import OAuth2PasswordBearer
+
+
+# JWT_SECRET = os.getenv("AUTH_SECRET_KEY")
+# JWT_ALGORITHM = os.getenv("AUTH_ALGORITHM")
+
+
+# def token_response(token: str):
+#     return {
+#         "access_token": token
+#     }
+
+
+# def signJWT(user_id: str) -> Dict[str, str]:
+#     payload = {
+#         "user_id": user_id,
+#         "expires": time.time() + 600
+#     }
+#     token = jwt.encode(payload, JWT_SECRET, algorithm=JWT_ALGORITHM)
+
+#     return token_response(token)
+
+
+# # def decodeJWT(token: str) -> dict:
+# #     try:
+# #         decoded_token = jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
+# #         # user = client.table("users").select("*").eq("id", decoded_token["user_id"]).execute()
+# #         # return decoded_token if user.data and decoded_token["expires"] >= time.time() else None
+# #         return decoded_token if decoded_token["expires"] >= time.time() else None
+# #     except:
+# #         return {}
+
+
+# def decodeJWT(token: str) -> dict:
+#     try:
+#         decoded_token = jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
+#         return decoded_token if decoded_token["expires"] >= time.time() else None
+#     except:
+#         return {}
+    
+
 import time
 from typing import Dict
+import os
 
 import jwt
-import os
 
 
 JWT_SECRET = os.getenv("AUTH_SECRET_KEY")
@@ -29,6 +78,7 @@ def signJWT(user_id: str) -> Dict[str, str]:
 def decodeJWT(token: str) -> dict:
     try:
         decoded_token = jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
-        return decoded_token if decoded_token["expires"] >= time.time() else None
+        # return decoded_token if decoded_token["expires"] >= time.time() else None
+        return decoded_token 
     except:
         return {}
--- backend/app/routers/users.py
+++ backend/app/routers/users.py
@@ -2,11 +2,22 @@ from fastapi import APIRouter, Depends
 from supabase import Client
 
 from app.dependencies import get_supa_client
-
+import jwt
+import os
 router = APIRouter()
+from ..dependencies.auth_bearer import JWTBearer
+
+JWT_SECRET = os.getenv("AUTH_SECRET_KEY")
+JWT_ALGORITHM = os.getenv("AUTH_ALGORITHM")
 
 
 @router.get("/")
 async def read_users(client: Client = Depends(get_supa_client)):
     res = client.table("users").select("*").execute()
     return res
+
+@router.get("/test")
+async def test_token(auth_token):
+    decoded_token = jwt.decode(auth_token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
+        # return decoded_token if decoded_token["expires"] >= time.time() else None
+    return decoded_token 
