38688f796b4a8ba5aacd500c8b6d5afba69a492c
magiciansz <80561550+magiciansz@users.noreply.github.com>
2024-01-20 15:07:56 +0800

    added destination API

--- backend/app/routers/users.py
+++ backend/app/routers/users.py
@@ -7,6 +7,7 @@ import os
 router = APIRouter()
 from ..dependencies.auth_bearer import JWTBearer
 
+
 JWT_SECRET = os.getenv("AUTH_SECRET_KEY")
 JWT_ALGORITHM = os.getenv("AUTH_ALGORITHM")
 
@@ -18,10 +19,20 @@ async def read_users(client: Client = Depends(get_supa_client)):
 @router.get("/{id}")
 async def get_user_itinerary(id:int, client: Client = Depends(get_supa_client)):
     user_data = client.table("users").select("*").eq("id", id).execute()
-    user_id = user_data.data[0]['id']
-    user_itinerary = client.table('itinerary').select("*").eq("user_id", user_id).execute()
-
-    return user_itinerary
+    if not user_data.data:
+        raise HTTPException(status_code=404, detail="User Not Found")
+    user_itinerary = client.table('itinerary').select("id").eq("user_id", id).execute()
+    if not user_itinerary.data:
+        return {}
+    arrayOfItineraryIDs = []
+    for itinerary in user_itinerary.data:
+        arrayOfItineraryIDs.append(itinerary['id'])
+    destination_itineraries = client.table('itinerary_destination').select("destination_id").in_('itinerary_id', arrayOfItineraryIDs).execute()
+    arrayOfDestinationIDs = []
+    for destination in destination_itineraries.data:
+        arrayOfDestinationIDs.append(destination['destination_id'])
+    destinations = client.table('destination').select("*").in_('id', arrayOfDestinationIDs).execute()
+    return destinations
 
 
     #return Response(status_code=200)
