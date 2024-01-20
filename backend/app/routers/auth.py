3522450c3dbebb6b227a3675d2bb8754ac1e4e54
Ryan Cheung <ryan9cheung@gmail.com>
2024-01-20 11:33:55 +0800

    Clean up auth responses

--- backend/app/routers/auth.py
+++ backend/app/routers/auth.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException, Response
+from fastapi.responses import JSONResponse
 from supabase import Client
 from pydantic import BaseModel
 from app.models.authModel import LoginBody
@@ -14,13 +15,13 @@ bcrypt = Bcrypt()
 @router.post("/login")
 async def login(body: LoginBody, client: Client = Depends(get_supa_client)):
     res = client.table("users").select("*").eq('username', body.username).execute()
-    # res = client.table("users").select("*").execute()
     if not res.data:
         raise HTTPException(status_code=404, detail="User not found")
     if bcrypt.check_password_hash(res.data[0]['password'], body.password):
-        return "Successful"
+        return JSONResponse(status_code=200, content={"data": "Success"})
     else:
         raise HTTPException(status_code=401, detail="Password is not correct")
+    
 @router.post("/register")
 async def register(body: RegisterBody, client: Client = Depends(get_supa_client)):
     first_name, last_name, username, password = body.first_name, body.last_name, body.username, body.password
@@ -32,7 +33,3 @@ async def register(body: RegisterBody, client: Client = Depends(get_supa_client)
     newUser = RegisterBody(first_name= first_name, last_name= last_name, username= username, password= hash)
     client.table('users').insert(jsonable_encoder(newUser)).execute()
     return Response(status_code=201)
-
-
-# @router.post('/register')
-# async def register(client: Client = Depends(get_supa_client)):
