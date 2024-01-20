0a70f5324a5f346a82d50cb1bf1e56b987872382
Ryan Cheung <ryan9cheung@gmail.com>
2024-01-20 13:56:56 +0800

    Bugfix auth handler

--- backend/app/dependencies/auth_handler.py
+++ backend/app/dependencies/auth_handler.py
@@ -1,75 +1,21 @@
-
-# import time
-# from typing import Dict
-# from fastapi import Depends
-
-# import jwt
-# import os
-# from supabase import Client
-# from app.dependencies import get_supa_client
-# from fastapi.security import OAuth2PasswordBearer
-
-
-# JWT_SECRET = os.getenv("AUTH_SECRET_KEY")
-# JWT_ALGORITHM = os.getenv("AUTH_ALGORITHM")
-
-
-# def token_response(token: str):
-#     return {
-#         "access_token": token
-#     }
-
-
-# def signJWT(user_id: str) -> Dict[str, str]:
-#     payload = {
-#         "user_id": user_id,
-#         "expires": time.time() + 600
-#     }
-#     token = jwt.encode(payload, JWT_SECRET, algorithm=JWT_ALGORITHM)
-
-#     return token_response(token)
-
-
-# # def decodeJWT(token: str) -> dict:
-# #     try:
-# #         decoded_token = jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
-# #         # user = client.table("users").select("*").eq("id", decoded_token["user_id"]).execute()
-# #         # return decoded_token if user.data and decoded_token["expires"] >= time.time() else None
-# #         return decoded_token if decoded_token["expires"] >= time.time() else None
-# #     except:
-# #         return {}
-
-
-# def decodeJWT(token: str) -> dict:
-#     try:
-#         decoded_token = jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
-#         return decoded_token if decoded_token["expires"] >= time.time() else None
-#     except:
-#         return {}
-    
-
 import time
 from typing import Dict
+from dotenv import load_dotenv
 import os
 
 import jwt
 
-
+load_dotenv()
 JWT_SECRET = os.getenv("AUTH_SECRET_KEY")
 JWT_ALGORITHM = os.getenv("AUTH_ALGORITHM")
 
 
 def token_response(token: str):
-    return {
-        "access_token": token
-    }
+    return {"access_token": token}
 
 
 def signJWT(user_id: str) -> Dict[str, str]:
-    payload = {
-        "user_id": user_id,
-        "expires": time.time() + 600
-    }
+    payload = {"user_id": user_id, "expires": time.time() + 600}
     token = jwt.encode(payload, JWT_SECRET, algorithm=JWT_ALGORITHM)
 
     return token_response(token)
@@ -79,6 +25,6 @@ def decodeJWT(token: str) -> dict:
     try:
         decoded_token = jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
         # return decoded_token if decoded_token["expires"] >= time.time() else None
-        return decoded_token 
+        return decoded_token
     except:
         return {}
